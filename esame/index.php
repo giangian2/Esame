9850761b7ba9df1ed1b1926728509e90d645399e
giangian2 <77266941+giangian2@users.noreply.github.com>
2021-05-28 13:09:18 +0200

    Update index.php

--- esame/index.php
+++ esame/index.php
@@ -55,8 +55,7 @@ include ("objects/sidebar.php");
                         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
                             <h5 class="text-uppercase">Info</h5>
                                 <p>
-                                    Dio boia dio boia dio boia dio boia dio boia dio boia dio boia
-                                    dio boia dio boia dio boia dio boia dio boia
+                                  
                                 </p>
                         </div>
 
